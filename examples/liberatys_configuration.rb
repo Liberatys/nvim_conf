bc87d1f28a27d409884f00816d2f433f5e019951
Liberatys <nick.flueckiger@renuo.ch>
2021-12-28 18:06:07 +0100

    Lintq

--- examples/liberatys_configuration.rb
+++ examples/liberatys_configuration.rb
@@ -1,8 +1,8 @@
-require 'pathname'
+require "pathname"
 
 NvimConf::Core.define do
   configuration do
-    output_folder '~/.config/nvim'
+    output_folder "~/.config/nvim"
     code_output :lua
     documented true
     write true
@@ -11,12 +11,12 @@ NvimConf::Core.define do
 
   commands do
     new(
-      'Unset space',
+      "Unset space",
       body: "vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })"
     )
 
     new(
-      'External Commands',
+      "External Commands",
       vim_exec: true,
       body: <<~COMMAND_BODY
         function! ExternalCommandResult()
@@ -30,7 +30,7 @@ NvimConf::Core.define do
 
   commands do
     new(
-      'Setup comments per filetype',
+      "Setup comments per filetype",
       vim_exec: true,
       body: <<~COMMAND_BODY
         augroup commenting_blocks_of_code
@@ -46,7 +46,7 @@ NvimConf::Core.define do
     )
 
     new(
-      'Force highligh for files',
+      "Force highligh for files",
       vim_exec: true,
       body: <<~COMMAND_BODY
         augroup hightlight_file_type
@@ -62,105 +62,105 @@ NvimConf::Core.define do
 
   plugins(:packer, bootstraped: true) do
     # LSP
-    plug 'dsummersl/nvim-sluice'
-    plug 'neovim/nvim-lspconfig'
-    plug 'ray-x/lsp_signature.nvim'
-    plug 'williamboman/nvim-lsp-installer'
-    plug 'onsails/lspkind-nvim'
+    plug "dsummersl/nvim-sluice"
+    plug "neovim/nvim-lspconfig"
+    plug "ray-x/lsp_signature.nvim"
+    plug "williamboman/nvim-lsp-installer"
+    plug "onsails/lspkind-nvim"
 
     # Navigation
-    plug 'pechorin/any-jump.vim'
+    plug "pechorin/any-jump.vim"
 
     # Completion
-    plug 'hrsh7th/nvim-cmp'
-    plug 'hrsh7th/cmp-buffer'
-    plug 'hrsh7th/cmp-path'
-    plug 'hrsh7th/cmp-nvim-lua'
-    plug 'hrsh7th/cmp-nvim-lsp'
-    plug 'tamago324/cmp-zsh'
-    plug 'f3fora/cmp-spell'
+    plug "hrsh7th/nvim-cmp"
+    plug "hrsh7th/cmp-buffer"
+    plug "hrsh7th/cmp-path"
+    plug "hrsh7th/cmp-nvim-lua"
+    plug "hrsh7th/cmp-nvim-lsp"
+    plug "tamago324/cmp-zsh"
+    plug "f3fora/cmp-spell"
 
     # Snippets
-    plug 'L3MON4D3/LuaSnip'
-    plug 'saadparwaiz1/cmp_luasnip'
-    plug 'michaelb/sniprun'
+    plug "L3MON4D3/LuaSnip"
+    plug "saadparwaiz1/cmp_luasnip"
+    plug "michaelb/sniprun"
 
     # Languages
-    plug 'elixir-editors/vim-elixir', file_types: 'elixir'
-    plug 'rust-lang/rust.vim', file_types: 'rust'
-    plug 'vim-ruby/vim-ruby', file_types: 'ruby'
-    plug 'tpope/vim-rails', file_types: 'ruby'
-    plug 'danchoi/ri.vim', file_types: 'ruby' # Searcher for ruby lang
-    plug 'slim-template/vim-slim', file_types: 'slim'
-    plug 'ElmCast/elm-vim', file_types: 'elm'
+    plug "elixir-editors/vim-elixir", file_types: "elixir"
+    plug "rust-lang/rust.vim", file_types: "rust"
+    plug "vim-ruby/vim-ruby", file_types: "ruby"
+    plug "tpope/vim-rails", file_types: "ruby"
+    plug "danchoi/ri.vim", file_types: "ruby" # Searcher for ruby lang
+    plug "slim-template/vim-slim", file_types: "slim"
+    plug "ElmCast/elm-vim", file_types: "elm"
 
     # Utility
-    plug 'tpope/vim-dispatch'
-    plug 'nathom/filetype.nvim'
-    plug 'lewis6991/impatient.nvim'
-    plug 'lewis6991/spellsitter.nvim'
-    plug 'tpope/vim-repeat'
-    plug 'tpope/vim-abolish'
-    plug 'tpope/vim-surround'
-    plug 'qwertologe/nextval.vim'
-    plug 'nvim-lua/plenary.nvim'
-    plug 'andymass/vim-matchup'
-    plug 'ggandor/lightspeed.nvim'
-    plug 'jiangmiao/auto-pairs'
-    plug 'lewis6991/gitsigns.nvim'
-    plug 'nvim-telescope/telescope-fzy-native.nvim'
-    plug 'nvim-telescope/telescope.nvim'
-    plug 'luukvbaal/stabilize.nvim'
-    plug 'nvim-treesitter/nvim-treesitter'
-    plug 'terrortylor/nvim-comment'
+    plug "tpope/vim-dispatch"
+    plug "nathom/filetype.nvim"
+    plug "lewis6991/impatient.nvim"
+    plug "lewis6991/spellsitter.nvim"
+    plug "tpope/vim-repeat"
+    plug "tpope/vim-abolish"
+    plug "tpope/vim-surround"
+    plug "qwertologe/nextval.vim"
+    plug "nvim-lua/plenary.nvim"
+    plug "andymass/vim-matchup"
+    plug "ggandor/lightspeed.nvim"
+    plug "jiangmiao/auto-pairs"
+    plug "lewis6991/gitsigns.nvim"
+    plug "nvim-telescope/telescope-fzy-native.nvim"
+    plug "nvim-telescope/telescope.nvim"
+    plug "luukvbaal/stabilize.nvim"
+    plug "nvim-treesitter/nvim-treesitter"
+    plug "terrortylor/nvim-comment"
 
     # Writing
-    plug 'folke/zen-mode.nvim'
+    plug "folke/zen-mode.nvim"
 
     # Documentation
-    plug 'skanehira/preview-uml.vim'
-    plug 'aklt/plantuml-syntax'
-    plug 'ellisonleao/glow.nvim'
+    plug "skanehira/preview-uml.vim"
+    plug "aklt/plantuml-syntax"
+    plug "ellisonleao/glow.nvim"
 
     # Refactoring
-    plug 'nvim-treesitter/nvim-treesitter-refactor'
-    plug 'nvim-treesitter/nvim-treesitter-textobjects'
-    plug 'windwp/nvim-spectre'
+    plug "nvim-treesitter/nvim-treesitter-refactor"
+    plug "nvim-treesitter/nvim-treesitter-textobjects"
+    plug "windwp/nvim-spectre"
 
     # Testing
-    plug 'vim-test/vim-test'
-    plug 'kassio/neoterm'
+    plug "vim-test/vim-test"
+    plug "kassio/neoterm"
 
     # Tabline / Statusline
-    plug 'folke/trouble.nvim'
-    plug 'tamton-aquib/staline.nvim'
+    plug "folke/trouble.nvim"
+    plug "tamton-aquib/staline.nvim"
 
     # Formating
-    plug 'sbdchd/neoformat'
+    plug "sbdchd/neoformat"
 
     # Linting
-    plug 'mfussenegger/nvim-lint'
+    plug "mfussenegger/nvim-lint"
 
     # UI
-    plug 'morhetz/gruvbox'
-    plug 'folke/todo-comments.nvim'
-    plug 'folke/which-key.nvim'
-    plug 'kevinhwang91/nvim-hlslens'
-    plug 'kyazdani42/nvim-web-devicons'
+    plug "morhetz/gruvbox"
+    plug "folke/todo-comments.nvim"
+    plug "folke/which-key.nvim"
+    plug "kevinhwang91/nvim-hlslens"
+    plug "kyazdani42/nvim-web-devicons"
   end
 
   settings do
     # General
     set :tabstop, 2
-    set :colorcolumn, '120'
+    set :colorcolumn, "120"
     set :relativenumber, true
     set :showcmd, true
-    set :encoding, 'utf-8'
+    set :encoding, "utf-8"
     set :updatetime, 300
     set :timeoutlen, 100
     set :synmaxcol, 200
     set :linebreak, false
-    set :bg, 'dark'
+    set :bg, "dark"
     set :mmp, 20_000
     set :expandtab, true
     set :shiftwidth, 2
@@ -180,8 +180,8 @@ NvimConf::Core.define do
       add :diffopt, opt_addition, scope: :opt
     end
 
-    add :clipboard, 'unnamedplus', scope: :opt
-    add :shortmess, 'c', scope: :opt
+    add :clipboard, "unnamedplus", scope: :opt
+    add :shortmess, "c", scope: :opt
 
     %w[
       *.o,*.obj,*~
@@ -214,19 +214,19 @@ NvimConf::Core.define do
     set :ignorecase, true
 
     # Shell
-    set :shell, '/bin/zsh'
-    set :undodir, Pathname.new('~/.vimdid').expand_path.to_s
+    set :shell, "/bin/zsh"
+    set :undodir, Pathname.new("~/.vimdid").expand_path.to_s
   end
 
   commands do
     new(
-      'Init gruvbox',
-      body: 'colorscheme gruvbox',
+      "Init gruvbox",
+      body: "colorscheme gruvbox",
       vim_exec: true
     )
 
     new(
-      'Init nvim_comment',
+      "Init nvim_comment",
       body: <<~COMMAND_BODY
         require('nvim_comment').setup({
           marker_padding = true,
@@ -240,7 +240,7 @@ NvimConf::Core.define do
     )
 
     new(
-      'Init dev_icons',
+      "Init dev_icons",
       body: <<~COMMAND_BODY
         require "nvim-web-devicons".setup {
             default = true
@@ -249,7 +249,7 @@ NvimConf::Core.define do
     )
 
     new(
-      'Init lightspeed',
+      "Init lightspeed",
       body: <<~COMMAND_BODY
         require'lightspeed'.setup {
           jump_on_partial_input_safety_timeout = 400,
@@ -268,7 +268,7 @@ NvimConf::Core.define do
     )
 
     new(
-      'Init tabline',
+      "Init tabline",
       body: <<~COMMAND_BODY
         require'stabline'.setup {
         	style = "slant",
@@ -280,7 +280,7 @@ NvimConf::Core.define do
     )
 
     new(
-      'Init testing',
+      "Init testing",
       vim_exec: true,
       body: <<~COMMAND_BODY
         let test#strategy = "neoterm"
@@ -288,11 +288,11 @@ NvimConf::Core.define do
     )
 
     {
-      comp: 'Init comp.nvim',
-      lsp: 'Init LSP',
-      tab_configuration: 'Init Tab Configuration',
-      telescope: 'Init Telescope',
-      treesitter: 'Init Treesitter'
+      comp: "Init comp.nvim",
+      lsp: "Init LSP",
+      tab_configuration: "Init Tab Configuration",
+      telescope: "Init Telescope",
+      treesitter: "Init Treesitter"
     }.each do |key, label|
       path = "#{__dir__}/configuration_parts/init_#{key}.lua"
       new(
@@ -303,15 +303,15 @@ NvimConf::Core.define do
   end
 
   requires do
-    setup('gitsigns')
-    setup('spellsitter')
-    setup('staline')
-    setup('todo-comments')
-    setup('which-key')
+    setup("gitsigns")
+    setup("spellsitter")
+    setup("staline")
+    setup("todo-comments")
+    setup("which-key")
   end
 
   globals do
-    set :mapleader, ' '
+    set :mapleader, " "
     set :neoformat_basic_format_align, true
     set :neoformat_basic_format_retab, true
     set :neoformat_basic_format_trim, true
@@ -340,50 +340,50 @@ NvimConf::Core.define do
       set "loaded_#{builtin_setting}", true
     end
 
-    set :neoformat_enabled_ruby, ['rubocop']
+    set :neoformat_enabled_ruby, ["rubocop"]
   end
 
   mappings do
-    map '<c-c>', '<ESC>'
-    map 'jk', '<ESC>'
-    nmap ';', ':'
-    vmap ';', ':'
+    map "<c-c>", "<ESC>"
+    map "jk", "<ESC>"
+    nmap ";", ":"
+    vmap ";", ":"
 
     # Disable arrow keys
-    map '<UP>', '<ESC>'
-    map '<DOWN>', '<ESC>'
-    map '<LEFT>', '<ESC>'
-    map '<RIGTH>', '<ESC>'
+    map "<UP>", "<ESC>"
+    map "<DOWN>", "<ESC>"
+    map "<LEFT>", "<ESC>"
+    map "<RIGTH>", "<ESC>"
 
     # tmap '<ESC>', '<C-\><C-n>'
     # nmap '<leader>ni', ':e $NOTE_INDEX/index.md<CR>:cd $NOTE_INDEX<CR>'
 
-    map '<Leader>rk', ':call ri#LookupNameUnderCursor()<CR>'
+    map "<Leader>rk", ":call ri#LookupNameUnderCursor()<CR>"
   end
 
   mappings do
-    map 'F', '^'
-    map 'T', '$'
+    map "F", "^"
+    map "T", "$"
 
-    map 'n', 'nzz'
-    map 'N', 'Nzz'
-    map '*', '*zz'
-    map '#', '#zz'
-    map 'g#', 'g*zz'
+    map "n", "nzz"
+    map "N", "Nzz"
+    map "*", "*zz"
+    map "#", "#zz"
+    map "g#", "g*zz"
 
-    vmap '<TAB>', '>gv'
-    vmap '<S-TAB>', '<gv'
+    vmap "<TAB>", ">gv"
+    vmap "<S-TAB>", "<gv"
 
-    nmap '<S-k>', 'mz:m-2<cr>`z'
-    nmap '<S-j>', 'mz:m+<cr>`z'
-    vmap '<S-j>', ":m'>+<cr>`<my`>mzgv`yo`z"
-    vmap '<S-k>', ":m'<-2<cr>`>my`<mzgv`yo`z"
+    nmap "<S-k>", "mz:m-2<cr>`z"
+    nmap "<S-j>", "mz:m+<cr>`z"
+    vmap "<S-j>", ":m'>+<cr>`<my`>mzgv`yo`z"
+    vmap "<S-k>", ":m'<-2<cr>`>my`<mzgv`yo`z"
   end
 
   mappings do
-    nmap '<C-J>', '<C-W><C-J>'
-    nmap '<C-K>', '<C-W><C-K>'
-    nmap '<C-H>', '<C-W><C-H>'
-    nmap '<c-s>', ':w<CR>'
+    nmap "<C-J>", "<C-W><C-J>"
+    nmap "<C-K>", "<C-W><C-K>"
+    nmap "<C-H>", "<C-W><C-H>"
+    nmap "<c-s>", ":w<CR>"
   end
 end
